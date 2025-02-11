3d9e089c3acae0a96f18b321a3f7c408f89cdbc7
onemangath <one.manga.th@gmail.com>
2025-02-11 13:49:40 +0700

    Add files via upload

--- api/checkout.js
+++ api/checkout.js
@@ -1,19 +1,11 @@
-require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'))
 
-const YOUR_DOMAIN = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Stripe Checkout Server!");
-});
-
-app.post('/create-checkout-session', async (req, res) => {
+app.post('/api/create-checkout-session', async (req, res) => {
+  console.log('📩 Received request body:', req.body);
   try {
     const { price_id } = req.body;
     if (!price_id) {
@@ -21,7 +13,7 @@ app.post('/create-checkout-session', async (req, res) => {
     }
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['promptpay'],
+      payment_method_types: ['card'],
       line_items: [
         {
           price: price_id,
@@ -29,14 +21,15 @@ app.post('/create-checkout-session', async (req, res) => {
         },
       ],
       mode: 'payment',
-      success_url: `${YOUR_DOMAIN}/success.html`,
-      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+      success_url: `${process.env.YOUR_DOMAIN}/success.html`,
+      cancel_url: `${process.env.YOUR_DOMAIN}/cancel.html`,
     });
 
     res.json({ url: session.url });
   } catch (error) {
     console.error("Error creating checkout session:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-app.listen(3000, () => console.log('🚀 Server running on http://localhost:3000'));
+
+module.exports = app;
